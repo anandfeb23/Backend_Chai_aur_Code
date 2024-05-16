83ab433c0bf4e7c8e4cef6e963ab6ec0ef17f73f
Himanshu Anand <work.anandhimanshu@gmail.com>
2024-05-16 23:51:28 +0530

    utils added

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+//Promise code used as a asyncHandler, its like a func, we don't have to write it again and again now, we can use it anywhere we want now, this code - 
+
+const asyncHandler = (requestHandler)=>{
+    (req, res, next)=> {
+        Promise.resolve(requestHandler(req,res, next)).
+        catch((err) => next(err))
+    }
+
+
+
+}
+
+export { asyncHandler }
+
+
+//try catch way to write func of asynchHandler -
+
+// const asyncHandler = (fn) => async (req,res,next)=>{
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+        
+//     }
+// }
